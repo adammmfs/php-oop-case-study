839573cbca84cf5ea2995c8d1c6b5004bbd8c004
adam fs <adammmfs@gmail.com>
2019-10-01 15:38:52 +0700

    Update AbstractMotor.php

--- src/Xtend/AbstractMotor.php
+++ src/Xtend/AbstractMotor.php
@@ -8,4 +8,13 @@ abstract class AbstractMotor
   abstract protected function getCc();
   abstract protected function getThn();
   abstract protected function getWarna();
+  
+  public function cetak(){
+    print $this->getTransmisi() . "\n";
+    print $this->getCc() . "\n";
+    print $this->getThn() . "\n";
+    print $this->getWarna() . "\n";
+  }
 }
+
+
