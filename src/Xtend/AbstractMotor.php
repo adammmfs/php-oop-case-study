f5994ab93c8af929b22353efe701106db0018709
adam fs <adammmfs@gmail.com>
2019-10-01 15:31:00 +0700

    Update AbstractMotor.php

--- src/Xtend/AbstractMotor.php
+++ src/Xtend/AbstractMotor.php
@@ -4,4 +4,8 @@ namespace Xtend;
 
 abstract class AbstractMotor
 {
+  abstract protected function getTransmisi();
+  abstract protected function getCc();
+  abstract protected function getThn();
+  abstract protected function getWarna();
 }
