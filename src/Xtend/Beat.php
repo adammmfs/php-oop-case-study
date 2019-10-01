0e6db00b96d841fad5b611010a3534498086b5da
adam fs <adammmfs@gmail.com>
2019-10-01 15:40:30 +0700

    Update Beat.php

--- src/Xtend/Beat.php
+++ src/Xtend/Beat.php
@@ -4,4 +4,16 @@ namespace Xtend;
 
 class Beat extends AbstractMotor
 {
+  public static function getTransmisi(){
+    return "matic";
+  }
+  public static function getCc(){
+    return "100";
+  }
+  public static function getThn(){
+    return "2015";
+  }
+  public static function getWarna(){
+    return "merah";
+  }
 }
