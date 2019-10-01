0b43f2565b31299a95e187c521f1d8e115616329
adam fs <adammmfs@gmail.com>
2019-10-01 15:42:09 +0700

    Update Mio125.php

--- src/Xtend/Mio125.php
+++ src/Xtend/Mio125.php
@@ -4,4 +4,16 @@ namespace Xtend;
 
 class Mio125 extends AbstractMotor
 {
+  public static function getTransmisi(){
+    return "matic";
+  }
+  public static function getCc(){
+    return "125";
+  }
+  public static function getThn(){
+    return "2017";
+  }
+  public static function getWarna(){
+    return "merah";
+  }
 }
