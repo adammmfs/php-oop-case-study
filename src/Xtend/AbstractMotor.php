06ab1fd3fe4c14e3f5af0ca10ef9f2b443177475
adam fs <adammmfs@gmail.com>
2019-10-02 16:40:18 +0700

    Update AbstractMotor.php

--- src/Xtend/AbstractMotor.php
+++ src/Xtend/AbstractMotor.php
@@ -13,6 +13,10 @@ abstract class AbstractMotor implements Spek
   abstract function setCc($cc);
   abstract function setThn($thn);
   
+  public function __construct(){
+      $this->setCc(100);
+  }
+  
 //   public function cetak(){
 //     print $this->getTransmisi() . "\n";
 //     print $this->getCc() . "\n";
